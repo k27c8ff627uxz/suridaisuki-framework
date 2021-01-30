b1302d05c229559995144e8ccbb95d30333ccf41
MasahiroSato <k27c8ff627uxz@yahoo.co.jp>
2021-01-31 01:03:21 +0900

    各トピックのタイトルとコメントを表示するようにする

--- pages/math/[topicId]/index.tsx
+++ pages/math/[topicId]/index.tsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
 import {
 	TopicList,
+	TopicData,
 } from '../../../utils/math_document';
 
 type Props = {
-	topic: string;
+	topicData: TopicData;
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
@@ -21,15 +24,27 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
 export const getStaticProps: GetStaticProps<Props> = async (ctx: GetStaticPropsContext) => {
 	const topic = ctx.params.topicId;
+	const topicData = require(`${process.env.DOCUMENT_PATH}/math/${topic}/data.json`) as TopicData;
 	return {
-		props: { topic: `Hello, ${topic}` },		
+		props: { topicData },		
 	};
 };
 
 export default class extends Component<Props> {
 	render() {
-		return (<div>
-			<h1>{this.props.topic}</h1>
+		return (<div style={{textAlign: 'center'}}>
+			<Head>
+				<title>{this.props.topicData.title}</title>
+			</Head>
+			<h1 style={{color: 'yellow'}}>{this.props.topicData.title}</h1>
+			<div style={{color: 'red'}}>
+				{ this.props.topicData.comment.map((text, index) => (
+					<div key={index}>{text}</div>
+				))}
+			</div>
+			<hr />
+			<hr />
+			<Link href="/math">戻る</Link>
 		</div>);
 	}
 }
--- utils/math_document.ts
+++ utils/math_document.ts
@@ -2,3 +2,8 @@
 export type TopicList = {
 	generating: string[];
 };
+
+export type TopicData = {
+	title: string;
+	comment: string[];
+};

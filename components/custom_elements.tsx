e36d10d2854874be83cb967c871d426385d0e3de
MasahiroSato <k27c8ff627uxz@yahoo.co.jp>
2021-02-17 01:44:16 +0900

    props.childrenを整理する処理の追加

--- components/custom_elements.tsx
+++ components/custom_elements.tsx
@@ -2,23 +2,61 @@ import React from 'react';
 import MathJax from 'react-mathjax';
 
 type Props = {
-	// props.childrenでとってこれないのが少し気持ち悪い
-	children: { props: { children: React.ReactNode }};
+	children: React.ReactNode;
+}
+
+function reformReactNode(node: React.ReactNode): React.ReactNode {
+	if (Array.isArray(node)) {
+		const ret_val = node
+			.map(reformReactNode)
+			.reduce<React.ReactNodeArray>((prev, current) => {
+				if (prev.length === 0) return [ current ];
+				const prevValue = prev[prev.length - 1];
+				if (typeof prevValue === 'string' && typeof current === 'string') {
+					prev[prev.length - 1] = prevValue + current;
+					return prev;
+				}
+				return [ ...prev, current ];
+			}, []);
+		if (ret_val.length === 0) throw `Not Implemented ${node}`;
+		if (ret_val.length === 1) return ret_val[0];
+		return ret_val;
+	} else if (node instanceof Object && 'props' in node) {
+		if (node.type.toString() === 'Symbol(react.fragment)') {
+			return node.props.children;
+		} else {
+			return {
+				...node,
+				props: {
+					...node.props,
+					children: reformReactNode(node.props.children),
+				},
+			};
+		}
+	} else if (typeof node === 'string') {
+		return node;
+	} else if (typeof node === 'number' ) {
+		return node.toString();
+	} else if (node === undefined) {
+		return '';
+	} else if (typeof node === 'boolean' || typeof node === 'function') {
+		throw `Not Implemented: ${node}`;
+	}
 }
 
 export const pic = (topicId: string) => (
 	({ children }: Props) => {
-		const { props } = children;
+		const picName = reformReactNode(children);
+		if (typeof picName !== 'string') throw `Error: <pic>${children}</pic>`;
 		return (
-			<img src={`${process.env.BASE_PATH}/math/${topicId}/pic/${props.children}`}/>
+			<img src={`${process.env.BASE_PATH}/math/${topicId}/pic/${picName}`}/>
 		);
 	}
 );
 
 export function exp({ children }: Props) {
-	const { props } = children;
-	if (props === undefined) return <div />;
-	const formula = props.children as string;
+	const formula = reformReactNode(children);
+	if (typeof formula !== 'string') throw `Error: <pic>${children}</pic>`;
 	return (
 		<MathJax.Provider>
 			<MathJax.Node inline formula={formula} />
